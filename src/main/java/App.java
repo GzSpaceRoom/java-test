91e55fa2eba7d5d323e9faac46473811b694619c
Mason <fanguizhang@163.com>
2019-12-24 21:04:40 +0800

    添加lombok 1.16.20，非最新版本，添加对lombok的简单特性测试代码

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) {
+        LombokGetterSetterExample x = new LombokGetterSetterExample();
+        System.out.println("Hello World");
+        System.out.println(x);
+    }
+}
